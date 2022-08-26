61c0b50f9a85969e4a28ee9c8580036014b8ab21
Jide_O <45870109+JideOgunlana@users.noreply.github.com>
2022-08-27 01:29:49 +0200

    updated working
    Global variable was changed to  a pointer and single file needs to be modularized

--- solong/test.c
+++ solong/test.c
@@ -71,7 +71,6 @@ typedef struct s_map_parsing
 typedef struct s_map_data
 {
 	char	*arr[10000000];
-	char	*ber_file;
 	int 	player_location_x;
 	int		player_location_y;
 	int		movement_count;
@@ -83,7 +82,8 @@ typedef struct s_map_data
 
 
 t_game_image	*g_images;
-t_map_data		g_m_data;
+t_map_data		*g_m_data;
+char			*ber_file;
 
 int	wall_in_w_direction(int wall_index)
 {
@@ -197,7 +197,7 @@ int	player_wins(int grabbed_eggs, int egg_index, int i, int j)
 {
 	if (grabbed_eggs == g_images->collectible->count)
 	{
-		if(g_m_data.arr[i][j] == 'E')
+		if(g_m_data->arr[i][j] == 'E')
 			if (player_on_exit_tile(egg_index))
 				return 1;
 	}
@@ -209,23 +209,23 @@ int check_for_w_move(int grabbed_eggs, int egg_index, int wall_index)
 	t_params params;
 
 	params.i = -1;
-	while (g_m_data.arr[++params.i] != 0)
+	while (g_m_data->arr[++params.i] != 0)
 	{
 		params.j = -1;
-		while (g_m_data.arr[params.i][++params.j] != '\0')
+		while (g_m_data->arr[params.i][++params.j] != '\0')
 		{
 			if (player_wins(grabbed_eggs, egg_index, params.i, params.j))
 			{
 				printf("You saved the \"Dinosaurs\"\n");
 				exit(1);
 			}
-			if (g_m_data.arr[params.i][params.j] == 'C')
+			if (g_m_data->arr[params.i][params.j] == 'C')
 			{
 				if (player_on_collectible_tile(egg_index))
 					break ;
 				egg_index += 1;
 			}
-			if (g_m_data.arr[params.i][params.j] == '1')
+			if (g_m_data->arr[params.i][params.j] == '1')
 			{
 				if (wall_in_w_direction(wall_index))
 					return 0;
@@ -242,23 +242,23 @@ int check_for_s_move(int grabbed_eggs, int egg_index, int wall_index)
 	t_params params;
 
 	params.i = -1;
-	while (g_m_data.arr[++params.i] != 0)
+	while (g_m_data->arr[++params.i] != 0)
 	{
 		params.j = -1;
-		while (g_m_data.arr[params.i][++params.j] != '\0')
+		while (g_m_data->arr[params.i][++params.j] != '\0')
 		{
 			if (player_wins(grabbed_eggs, egg_index, params.i, params.j))
 			{
 				printf("You saved the \"Dinosaurs\"\n");
 				exit(1);
 			}
-			if (g_m_data.arr[params.i][params.j] == 'C')
+			if (g_m_data->arr[params.i][params.j] == 'C')
 			{
 				if (player_on_collectible_tile(egg_index))
 					break ;
 				egg_index += 1;
 			}
-			if (g_m_data.arr[params.i][params.j] == '1')
+			if (g_m_data->arr[params.i][params.j] == '1')
 			{
 				if (wall_in_s_direction(wall_index))
 					return 0;
@@ -274,23 +274,23 @@ int check_for_a_move(int grabbed_eggs, int egg_index, int wall_index)
 	t_params params;
 
 	params.i = -1;
-	while (g_m_data.arr[++params.i] != 0)
+	while (g_m_data->arr[++params.i] != 0)
 	{
 		params.j = -1;
-		while (g_m_data.arr[params.i][++params.j] != '\0')
+		while (g_m_data->arr[params.i][++params.j] != '\0')
 		{
 			if (player_wins(grabbed_eggs, egg_index, params.i, params.j))
 			{
 				printf("You saved the \"Dinosaurs\"\n");
 				exit(1);
 			}
-			if (g_m_data.arr[params.i][params.j] == 'C')
+			if (g_m_data->arr[params.i][params.j] == 'C')
 			{
 				if (player_on_collectible_tile(egg_index))
 					break ;
 				egg_index += 1;
 			}
-			if (g_m_data.arr[params.i][params.j] == '1')
+			if (g_m_data->arr[params.i][params.j] == '1')
 			{
 				if (wall_in_a_direction(wall_index))
 					return 0;
@@ -306,23 +306,23 @@ int check_for_d_move(int grabbed_eggs, int egg_index, int wall_index)
 	t_params params;
 
 	params.i = -1;
-	while (g_m_data.arr[++params.i] != 0)
+	while (g_m_data->arr[++params.i] != 0)
 	{
 		params.j = -1;
-		while (g_m_data.arr[params.i][++params.j] != '\0')
+		while (g_m_data->arr[params.i][++params.j] != '\0')
 		{
 			if (player_wins(grabbed_eggs, egg_index, params.i, params.j))
 			{
 				printf("You saved the \"Dinosaurs\"\n");
 				exit(1);
 			}
-			if (g_m_data.arr[params.i][params.j] == 'C')
+			if (g_m_data->arr[params.i][params.j] == 'C')
 			{
 				if (player_on_collectible_tile(egg_index))
 					break ;
 				egg_index += 1;
 			}
-			if (g_m_data.arr[params.i][params.j] == '1')
+			if (g_m_data->arr[params.i][params.j] == '1')
 			{
 				if (wall_in_d_direction(wall_index))
 					return 0;
@@ -339,10 +339,10 @@ void	w_key_pressed(int grabbed_eggs, int egg_index, int wall_index)
 
 	grabbed_eggs += picked_collectibles_count(g_images->collectible->count, grabbed_eggs);
 	move = check_for_w_move(grabbed_eggs, egg_index, wall_index);
-	if (move && g_m_data.movement_count++)
+	if (move && g_m_data->movement_count++)
 		g_images->player->instances[0].y -= 4;
 		ft_putstr_fd("moves: ", STDOUT_FILENO);
-		ft_putnbr_fd(g_m_data.movement_count, STDOUT_FILENO);
+		ft_putnbr_fd(g_m_data->movement_count, STDOUT_FILENO);
 		ft_putchar_fd('\n', STDOUT_FILENO);
 }
 
@@ -352,10 +352,10 @@ void	s_key_pressed(int grabbed_eggs, int egg_index, int wall_index)
 
 	grabbed_eggs += picked_collectibles_count(g_images->collectible->count, grabbed_eggs);
 	move = check_for_s_move(grabbed_eggs,  egg_index, wall_index);
-	if (move && g_m_data.movement_count++)
+	if (move && g_m_data->movement_count++)
 		g_images->player->instances[0].y += 4;
 		ft_putstr_fd("moves: ", STDOUT_FILENO);
-		ft_putnbr_fd(g_m_data.movement_count, STDOUT_FILENO);
+		ft_putnbr_fd(g_m_data->movement_count, STDOUT_FILENO);
 		ft_putchar_fd('\n', STDOUT_FILENO);
 }
 
@@ -365,10 +365,10 @@ void	a_key_pressed(int grabbed_eggs, int egg_index, int wall_index)
 
 	grabbed_eggs += picked_collectibles_count(g_images->collectible->count, grabbed_eggs);
 	move = check_for_a_move(grabbed_eggs,  egg_index, wall_index);
-	if (move && g_m_data.movement_count++)
+	if (move && g_m_data->movement_count++)
 		g_images->player->instances[0].x -= 4;
 		ft_putstr_fd("moves: ", STDOUT_FILENO);
-		ft_putnbr_fd(g_m_data.movement_count, STDOUT_FILENO);
+		ft_putnbr_fd(g_m_data->movement_count, STDOUT_FILENO);
 		ft_putchar_fd('\n', STDOUT_FILENO);
 }
 
@@ -378,10 +378,10 @@ void	d_key_pressed(int grabbed_eggs, int egg_index, int wall_index)
 
 	grabbed_eggs += picked_collectibles_count(g_images->collectible->count, grabbed_eggs);
 	move = check_for_d_move(grabbed_eggs,  egg_index, wall_index);
-	if (move && g_m_data.movement_count++)
+	if (move && g_m_data->movement_count++)
 		g_images->player->instances[0].x += 4;
 		ft_putstr_fd("moves: ", STDOUT_FILENO);
-		ft_putnbr_fd(g_m_data.movement_count, STDOUT_FILENO);
+		ft_putnbr_fd(g_m_data->movement_count, STDOUT_FILENO);
 		ft_putchar_fd('\n', STDOUT_FILENO);
 }
 
@@ -446,8 +446,8 @@ void	create_g_images(t_game_image* g_images, mlx_t *mlx, t_texture *val)
 
 void	get_player_location(t_map_parsing *p_map)
 {
-		g_m_data.player_location_x = 48 * p_map->x;
-		g_m_data.player_location_y = 48 * p_map->y;
+		g_m_data->player_location_x = 48 * p_map->x;
+		g_m_data->player_location_y = 48 * p_map->y;
 		p_map->player_count++;
 }
 
@@ -487,9 +487,9 @@ void	parse_game_map(mlx_t *mlx, t_map_parsing *p_map)
 		p_map->line = get_next_line(p_map->fd);
 		if (!p_map->line)
 			break ;
-		g_m_data.arr[p_map->y] = p_map->line;
+		g_m_data->arr[p_map->y] = p_map->line;
 		check_line(mlx, p_map);
-		g_m_data.line_count += 1;
+		g_m_data->line_count += 1;
 		p_map->y++;
 	}
 	if (p_map->collectible_count < 1 || p_map->player_count != 1 || p_map->escape_count != 1)
@@ -506,20 +506,20 @@ void	check_map_dimensions()
 	int i;
 
 	i = 0;
-	while (g_m_data.arr[i] != 0)
+	while (g_m_data->arr[i] != 0)
 	{
 		int j = 0;
-		while (g_m_data.arr[i][j] != '\0' && g_m_data.arr[i][j] != '\n')
+		while (g_m_data->arr[i][j] != '\0' && g_m_data->arr[i][j] != '\n')
 		{
-			if (i == 0 && g_m_data.arr[i][j + 1] == '\n')
-				g_m_data.map_width1 = j;
-			if (i > 0 && (g_m_data.arr[i][j + 1] == '\n' || g_m_data.arr[i][j + 1] == '\0'))
-				g_m_data.map_width2 = j;
-			if (g_m_data.map_width1 && g_m_data.map_width2)
+			if (i == 0 && g_m_data->arr[i][j + 1] == '\n')
+				g_m_data->map_width1 = j;
+			if (i > 0 && (g_m_data->arr[i][j + 1] == '\n' || g_m_data->arr[i][j + 1] == '\0'))
+				g_m_data->map_width2 = j;
+			if (g_m_data->map_width1 && g_m_data->map_width2)
 			{
-				if (g_m_data.map_width1 != g_m_data.map_width2)
+				if (g_m_data->map_width1 != g_m_data->map_width2)
 				{
-					printf("%d, %d:\n", g_m_data.map_width1, g_m_data.map_width2);
+					printf("%d, %d:\n", g_m_data->map_width1, g_m_data->map_width2);
 					ft_putstr_fd("Error\nNot a valid map\n", STDOUT_FILENO);
 					exit(1);
 				}
@@ -534,9 +534,9 @@ void	check_map_dimensions()
 void	is_top_wall_valid()
 {
 	int i = 0;
-	while (g_m_data.arr[0][i] != '\0' && g_m_data.arr[0][i] != '\n')
+	while (g_m_data->arr[0][i] != '\0' && g_m_data->arr[0][i] != '\n')
 	{
-		if (g_m_data.arr[0][i] != '1')
+		if (g_m_data->arr[0][i] != '1')
 		{
 			ft_putstr_fd("Error\nTop Outer wall is not valid\n", STDOUT_FILENO);
 			exit(1);
@@ -550,11 +550,11 @@ void	is_bottom_wall_valid()
 	int i;
 
 	i = 0;
-	g_m_data.last_line = g_m_data.line_count - 1;
+	g_m_data->last_line = g_m_data->line_count - 1;
 
-	while (g_m_data.arr[g_m_data.last_line][i] != '\0' && g_m_data.arr[g_m_data.last_line][i] != '\n')
+	while (g_m_data->arr[g_m_data->last_line][i] != '\0' && g_m_data->arr[g_m_data->last_line][i] != '\n')
 	{
-		if (g_m_data.arr[g_m_data.last_line][i] != '1')
+		if (g_m_data->arr[g_m_data->last_line][i] != '1')
 		{
 			ft_putstr_fd("Error\nbottom Outer wall is not valid\n", STDOUT_FILENO);
 			exit(1);
@@ -568,9 +568,9 @@ void	is_left_wall_valid()
 	int i;
 
 	i = 0;
-	while (i < g_m_data.line_count)
+	while (i < g_m_data->line_count)
 	{
-		if (g_m_data.arr[i][0] != '1')
+		if (g_m_data->arr[i][0] != '1')
 		{
 			ft_putstr_fd("Error\nLeft Outer wall is not valid\n", STDOUT_FILENO);
 			exit(1);
@@ -584,9 +584,9 @@ void	is_right_wall_valid()
 	int i;
 
 	i = 0;
-	while (i < g_m_data.line_count)
+	while (i < g_m_data->line_count)
 	{
-		if (g_m_data.arr[i][g_m_data.map_width1] != '1')
+		if (g_m_data->arr[i][g_m_data->map_width1] != '1')
 		{
 			ft_putstr_fd("Error\nRight outer wall is not valid\n", STDOUT_FILENO);
 			exit(1);
@@ -616,6 +616,7 @@ void	clean_up(mlx_t *mlx, t_texture *val)
 	mlx_delete_texture(val->escape_texture);
 	mlx_delete_texture(val->wall_texture);
 	free(g_images);
+	free(g_m_data);
 	free(val);
 }
 
@@ -626,15 +627,15 @@ void	set_up_game(mlx_t *mlx, t_texture *val, t_map_parsing *p_map)
 	mlx_image_to_window(mlx, g_images->bg, 0, 0);
 	parse_game_map(mlx, p_map);
 	check_map_dimensions();
-	if (g_m_data.line_count == g_m_data.map_width1 + 1)
+	if (g_m_data->line_count == g_m_data->map_width1 + 1)
 	{
 		ft_putstr_fd("Error\nMap is a square, it should be rectangular\n", STDOUT_FILENO);
 		exit(1);
 	}
 	check_walls();
 	mlx_loop_hook(mlx, &hook, mlx);
-	mlx_image_to_window(mlx, g_images->player, g_m_data.player_location_x, g_m_data.player_location_y);
-	mlx_set_window_size(mlx, (g_m_data.map_width1 + 1) * 48, g_m_data.line_count * 48);
+	mlx_image_to_window(mlx, g_images->player, g_m_data->player_location_x, g_m_data->player_location_y);
+	mlx_set_window_size(mlx, (g_m_data->map_width1 + 1) * 48, g_m_data->line_count * 48);
 	mlx_loop(mlx);
 	clean_up(mlx, val);
 	mlx_terminate(mlx);
@@ -648,7 +649,7 @@ void	start_game()
 	t_map_parsing	*p_map;
 
 	p_map = &map;
-	map.fd = open(g_m_data.ber_file, O_RDONLY);
+	map.fd = open(ber_file, O_RDONLY);
 	if (map.fd == -1)
 	{
 		perror("Error\n");
@@ -657,7 +658,8 @@ void	start_game()
 	mlx = mlx_init(WIDTH, HEIGHT, "Jurassic Age", true);
 	val = (t_texture*) malloc(sizeof(t_texture));
 	g_images = (t_game_image*) malloc(sizeof(t_game_image));
-	if (!g_images || !val || !mlx)
+	g_m_data = (t_map_data*) malloc(sizeof(t_map_data));
+	if (!g_images || !val || !mlx || !g_m_data)
 		return ;
 	set_up_game(mlx, val, p_map);
 }
@@ -679,7 +681,7 @@ int	main(int argc, char *argv[])
 				{
 					if (argv[1][params.i + 4] != '\0')
 						break;
-					g_m_data.ber_file = argv[1];
+					ber_file = argv[1];
 					params.valid_file = 1;
 					start_game();
 				}
